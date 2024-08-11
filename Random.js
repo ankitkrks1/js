b1cf7a2424c928125a333f9175c45cb2ad1eaa2e
Ankit Kumar Kashyap <ankitkrks@ankitkrks.local>
2024-08-11 18:39:29 +0530

    random

--- Random.js
+++ Random.js
@@ -6,6 +6,32 @@
  *  b. crypto.getRandomValue()
  */
 
-let random = Math.random(); // Returns the random no. including 0 and excluding 1. 
+var random = Math.random(); // Returns the random no. including 0 and excluding 1. 
 
 console.log(random);
+
+
+/**
+ *  Using Crypto to get the random . 
+ */
+
+ // Create a Uint32Array with one element
+ const randomBytes = new Uint32Array(1);
+
+ // Fill the array with cryptographically secure random values
+ crypto.getRandomValues(randomBytes);
+
+ // Generate a random number within the range
+ var  random = randomBytes[0] / (0xFFFFFFFF + 1); // Normalize to [0, 1)
+
+/**
+ * Random in a given range 
+ *  min -- max 
+ */
+
+const randomInRange = (min,max) =>{
+ 
+    return random*(max -min) +min
+}
+
+console.log(randomInRange(5,10))
